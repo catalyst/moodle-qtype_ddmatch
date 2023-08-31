acac3ab1d8473e7e34a520e467d307eb0dee9548
Peter Burnett <peterburnett@catalyst-au.net>
2023-09-01 09:29:36 +1000

    Rename provicy provider file to be autoloaded correctly

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -18,6 +18,7 @@
  * Privacy Subsystem implementation for format_tabtopics.
  *
  * @package    qtype_ddmatch
+ * 
  * @author DualCube <admin@dualcube.com>
  * @copyright  2007 DualCube (https://dualcube.com) 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -28,14 +29,11 @@ namespace qtype_ddmatch\privacy;
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Privacy Subsystem implementation for format_tabtopics.
+ * The format_tabtopics
  *
- * @package    qtype_ddmatch
- * @author DualCube <admin@dualcube.com>
- * @copyright  2007 DualCube (https://dualcube.com) 
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider{
+class provider implements \core_privacy\local\metadata\null_provider
+{
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
